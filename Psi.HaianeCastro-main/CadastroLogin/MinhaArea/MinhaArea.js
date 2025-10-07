de62fe39c5aa50ae1458b236ab3d37abce3d2e5e
Sarah69359 <sarah69359@estudante.ifb.edu.br>
2025-10-07 09:28:10 -0300

    Commit inicial do projeto

---
+++ Psi.HaianeCastro-main/CadastroLogin/MinhaArea/MinhaArea.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    
+    // Seleciona todos os links da navegação
+    const navLinks = document.querySelectorAll('.navigation-grid a');
+
+    // Adiciona um evento de clique para cada link
+
+
+});
